901e9c29082c1f0de657b46a4f63e0e1775d745e
Amitabh Ranjan <amitabh.march22@gmail.com>
2022-02-02 13:35:07 -0500

    create new Apollo server and integrate with Express application as middleware in server.js

--- server/server.js
+++ server/server.js
@@ -1,10 +1,26 @@
 const express = require('express');
+const { ApolloServer } = require('apollo-server-express');
+const { authMiddleware } = require('./utils/auth');
+
 const path = require('path');
 const db = require('./config/connection');
 const routes = require('./routes');
 
+//importing typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+//creating new Apollo server and pass in schemas data
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+})
+
+//integrating the Apollo server with Express application as middleware
+server.applyMiddleware({app});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
